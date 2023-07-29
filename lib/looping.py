5480dc2a5aa3d2e605097e74726957181b4aec3a
Harpreet Singh <hurpreet@icloud.com>
2023-07-29 16:38:29 -0400

    Lab Completed

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
+import math
 
 def happy_new_year():
     # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+
+    print("Happy New Year!")
+   
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    int_list= [math.pow(num, 2) for num in int_list]
+    return (int_list)
+   
 
 def fizzbuzz():
     # code goes here!
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i% 3 == 0:
+            print("Fizz")
+        elif i% 5 ==0:
+            print("Buzz")
+        else:
+            print(i)
     pass
